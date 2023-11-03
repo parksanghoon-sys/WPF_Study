f01ae79774036538bbfdc8a271aab670880ce57d
DESKTOP-DFB0ECA\shpark <shpark94@soletop.co.kr>
2023-11-03 17:51:37 +0900

    update

---
+++ wpfCustom.Lib/Custom.Wpf/Global/Controls/RepeatColorChangeToggleButton.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls.Primitives;
+
+namespace Custom.Wpf.Global.Controls
+{
+    public class RepeatColorChangeToggleButton : ToggleButton
+    {
+        static RepeatColorChangeToggleButton()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(RepeatColorChangeToggleButton), new FrameworkPropertyMetadata(typeof(RepeatColorChangeToggleButton)));
+        }
+    }
+}
