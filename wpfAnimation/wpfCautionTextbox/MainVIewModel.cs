a17c6d4e5b10742148fc453a60e62c7d831065b1
DESKTOP-DFB0ECA\shpark <shpark94@soletop.co.kr>
2023-12-18 10:23:44 +0900

    feat: 경고 텍스트박스 Tooltip Binding 가능하도록  업데이트 바인딩 오류 해결

---
+++ wpfAnimation/wpfCautionTextbox/MainVIewModel.cs
@@ -0,0 +1,50 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Custom.Wpf.Global.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace wpfCautionTextbox
+{
+    public partial class  MainVIewModel : ObservableBase
+    {
+        [ObservableProperty]
+        private bool _isCaution;
+        
+        private string? _cautionText;
+        public string CautionText
+        {
+            get => _cautionText!;
+            set
+            {                
+                SetProperty(ref _cautionText, value);
+            }
+        }
+        public ICommand SaveCommand { get; init; }
+        [RelayCommand]
+        private void OnBtnClick()
+        {
+            IsCaution = true;
+            CautionText = "경고 입력";
+            
+        }
+
+        private void Save()
+        {
+            IsCaution = !IsCaution;
+            CautionText = "경고 입력";
+        }
+
+        public MainVIewModel()
+        {
+            IsCaution = false;
+            SaveCommand = new RelayCommand(Save);
+          
+        }
+
+    }
+}
