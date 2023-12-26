169b44616d077c9f2620a42fc0aeac89a5f80343
DESKTOP-DFB0ECA\shpark <shpark94@soletop.co.kr>
2023-12-26 21:54:15 +0900

    Update

---
+++ wpfAnimation/wpfBarGaugeBar/MainViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wpfBarGaugeBar
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+
+        public MainViewModel()
+        {
+            
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        protected virtual void OnPropertyChanged([CallerMemberName]string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
