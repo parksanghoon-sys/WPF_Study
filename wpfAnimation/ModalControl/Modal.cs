15c0f582de0381a738ecdecae534f7af0ffc976f
DESKTOP-DFB0ECA\shpark <shpark94@soletop.co.kr>
2024-05-17 15:53:27 +0900

    update

---
+++ wpfAnimation/ModalControl/Modal.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace ModalControl
+{
+    public class Modal : ContentControl
+    {
+        public static readonly DependencyProperty IsOpenProperty =
+            DependencyProperty.Register("IsOpen", typeof(bool), typeof(Modal), new PropertyMetadata(false));
+        public bool IsOpen
+        {
+            get { return (bool)GetValue(IsOpenProperty); }
+            set { SetValue(IsOpenProperty, value); }
+        }
+
+
+        public object ContentTemplates
+        {
+            get { return (object)GetValue(ContentTemplatesProperty); }
+            set { SetValue(ContentTemplatesProperty, value); }
+        }
+        
+        public static readonly DependencyProperty ContentTemplatesProperty =
+            DependencyProperty.Register("ContentTemplates", typeof(object), typeof(Modal), new PropertyMetadata(null));
+
+
+        static Modal()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(Modal), new FrameworkPropertyMetadata(typeof(Modal)));
+            BackgroundProperty.OverrideMetadata(typeof(Modal), new FrameworkPropertyMetadata(CreateDefaultBackground()));
+        }
+
+        private static object CreateDefaultBackground()
+        {
+            return new SolidColorBrush(Colors.Black)
+            {
+                Opacity = 0.3
+            };
+        }
+    }
+}
