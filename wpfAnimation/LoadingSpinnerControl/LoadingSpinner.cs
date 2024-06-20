b56ffd27622329c45ccba198bddc6e2e303c8997
DESKTOP-DFB0ECA\shpark <shpark94@soletop.co.kr>
2024-06-20 13:34:10 +0900

    update

---
+++ wpfAnimation/LoadingSpinnerControl/LoadingSpinner.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace LoadingSpinnerControl
+{
+    public class LoadingSpinner : Control
+    {
+
+
+        public bool IsLoading
+        {
+            get { return (bool)GetValue(IsLoadingProperty); }
+            set { SetValue(IsLoadingProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for IsLoading.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty IsLoadingProperty =
+            DependencyProperty.Register("IsLoading", typeof(bool), typeof(LoadingSpinner), new PropertyMetadata(false));
+
+
+        public double Diameter
+        {
+            get { return (double)GetValue(DiameterProperty); }
+            set { SetValue(DiameterProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Diameter.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty DiameterProperty =
+            DependencyProperty.Register("Diameter", typeof(double), typeof(LoadingSpinner), new PropertyMetadata(100.0d));
+
+
+        public double Thickness
+        {
+            get { return (double)GetValue(ThicknessProperty); }
+            set { SetValue(ThicknessProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Thickness.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty ThicknessProperty =
+            DependencyProperty.Register("Thickness", typeof(double), typeof(LoadingSpinner), new PropertyMetadata(1.0d));
+
+
+        public Brush Color
+        {
+            get { return (Brush)GetValue(ColorProperty); }
+            set { SetValue(ColorProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for MyProperty.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty ColorProperty =
+            DependencyProperty.Register("Color", typeof(Brush), typeof(LoadingSpinner), new PropertyMetadata(Brushes.Black));
+        public PenLineCap Cap
+        {
+            get { return (PenLineCap)GetValue(CapProperty); }
+            set { SetValue(CapProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Cap.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty CapProperty =
+            DependencyProperty.Register("Cap", typeof(PenLineCap), typeof(LoadingSpinner), new PropertyMetadata(PenLineCap.Flat));
+        static LoadingSpinner()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(LoadingSpinner), new FrameworkPropertyMetadata(typeof(LoadingSpinner)));
+        }
+    }
+}
