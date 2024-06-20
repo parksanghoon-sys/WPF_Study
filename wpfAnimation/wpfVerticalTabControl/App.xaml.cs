0b762405329f409d2e092bab7e3f3c1270c2680b
DESKTOP-DFB0ECA\shpark <shpark94@soletop.co.kr>
2024-06-20 17:59:57 +0900

    test update

--- wpfAnimation/wpfVerticalTabControl/App.xaml.cs
+++ wpfAnimation/wpfVerticalTabControl/App.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System.Windows;
+﻿using CommunityToolkit.Mvvm.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+using Prism.Services.Dialogs;
+using System;
+using System.Windows;
 
 namespace wpfVerticalTabControl
 {
@@ -7,5 +11,28 @@ namespace wpfVerticalTabControl
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            var dialogService = Ioc.Default.GetService<IDialogService>();
+        }
+    }
+    public class Bootstrapper
+    {
+        public Bootstrapper()
+        {
+            var services = ConfigureServices();
+            Ioc.Default.ConfigureServices(services);
+        }
+
+        private static IServiceProvider ConfigureServices()
+        {
+            var services = new ServiceCollection();
+
+            // Services
+            services.AddSingleton<IDialogService, DialogService>();         
+
+            return services.BuildServiceProvider();
+        }
     }
 }
